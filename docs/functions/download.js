539f1bd5a9f574da5e4ea0d28e567a5f643a78ef
Wang Xiaoyu <wxy6987@outlook.com>
2026-09-18 06:49:05 +0800

    Update download.js

--- docs/functions/download.js
+++ docs/functions/download.js
@@ -1,5 +1,4 @@
-// functions/download.js
 export async function onRequest(context) {
   const assetUrl = new URL("/res/pylind.apk", context.request.url);
-  return context.env.ASSETS.fetch(assetUrl);
+  return Response.redirect(assetUrl.toString(), 302);
 }
